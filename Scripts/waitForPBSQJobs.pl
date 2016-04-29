555667d8b0870c3f928e28e860cce0c5e34ab828
stekaiser <kaiser.steffen@gmail.com>
2016-04-29 15:15:36 +0200

    Beautify perl code

--- Scripts/waitForPBSQJobs.pl
+++ Scripts/waitForPBSQJobs.pl
@@ -14,42 +14,40 @@ use strict;
 # Usual qstat format - check this at run time
 # job-ID  prior   name       user         state submit/start at     queue                          slots ja-task-ID
 
-
 # First thing to do is parse our input
 
 my ( $verbose, $delay, @jobIDs ) = @ARGV;
 
 # Check for user stupidity
-if( $delay < 10 )
-  {
-  print STDERR "Sleep period is too short, will poll queue once every 10 seconds\n";
-  $delay = 10;
-  }
-elsif( $delay > 3600 )
-  {
-  print STDERR "Sleep period is too long, will poll queue once every 60 minutes\n";
-  $delay = 3600;
-  }
+if ( $delay < 10 ) {
+    print STDERR "Sleep period is too short, will poll queue once every 10 seconds\n";
+    $delay = 10;
+}
+elsif ( $delay > 3600 ) {
+    print STDERR "Sleep period is too long, will poll queue once every 60 minutes\n";
+    $delay = 3600;
+}
 
-print "  Waiting for " . scalar( @jobIDs ) . " jobs: @jobIDs\n";
+print "  Waiting for " . scalar(@jobIDs) . " jobs: @jobIDs\n";
 
-my $user=`whoami`;
+my $user = `whoami`;
 
 my $qstatOutput = `qstat -u $user`;
 
-if( !scalar(@jobIDs) || !$qstatOutput )
-  {
-  # Nothing to do
-  exit 0;
-  }
+if ( !scalar(@jobIDs) || !$qstatOutput ) {
 
-my @qstatLines = split("\n", $qstatOutput);
+    # Nothing to do
+    exit 0;
+}
+
+my @qstatLines = split( "\n", $qstatOutput );
+
 # my @header = split('\s+', trim($qstatLines[0]));
 
 # Position in qstat output of tokens we want
 # Here we hardcode the values that work at UVa (alebac: also at DCCN; stekai: also at DCC)
 my $jobID_Pos = 0;
-my $statePos = 9;
+my $statePos  = 9;
 
 # foreach my $i (0..$#header)
 #   {
@@ -63,7 +61,6 @@ my $statePos = 9;
 #     }
 #   }
 
-
 # If we can't parse the job IDs, something is very wrong
 # if ($jobID_Pos < 0 || $statePos < 0)
 #   {
@@ -76,62 +73,56 @@ my $jobsIncomplete = 1;
 # Set to 1 for any job in an error state
 my $haveErrors = 0;
 
-while( $jobsIncomplete )
-  {
+while ($jobsIncomplete) {
 
-  # Jobs that are still showing up in qstat
-  $jobsIncomplete = 0;
+    # Jobs that are still showing up in qstat
+    $jobsIncomplete = 0;
 
-  foreach my $job (@jobIDs)
-    {
-    # iterate over all user jobs in the queue
-    qstatLine: foreach my $line ( @qstatLines )
-      {
-      # trim string for trailing white space so that the tokens are in the correct sequence
-      # We are being paranoid by matching tokens to job-IDs this way. Less elegant than a
-      # match but also less chance of a false-positive match
+    foreach my $job (@jobIDs) {
 
-      my @tokens = split( '\s+', trim( $line ) );
+        # iterate over all user jobs in the queue
+    qstatLine: foreach my $line (@qstatLines) {
 
-      # The qstat command only prints the first 15 characters of the job
-      # so we only compare the first 15 characters
-	  # alebac: for DCCN its 22 chars
-      # stekai: for DCC its 16 chars
+            # trim string for trailing white space so that the tokens are in the correct sequence
+            # We are being paranoid by matching tokens to job-IDs this way. Less elegant than a
+            # match but also less chance of a false-positive match
 
-      my $job_short = substr( $job, 0, 16 );
+            my @tokens = split( '\s+', trim($line) );
 
-	  # alebac: adapted the following lines to work at DCCN; stekai: works for DCC as well
-	  # if not empty & job from list & still running
-      if( @tokens > 0 && ( $tokens[$jobID_Pos] eq $job_short ) && ( $tokens[$statePos] ne "C" ) )
-        {
+            # The qstat command only prints the first 15 characters of the job
+            # so we only compare the first 15 characters
+            # alebac: for DCCN its 22 chars
+            # stekai: for DCC its 16 chars
 
-          $jobsIncomplete = $jobsIncomplete + 1;
+            my $job_short = substr( $job, 0, 16 );
 
-        if( $verbose )
-          {
-	  print "    Job $job is in state $tokens[$statePos]\n";
-	  }
-	}
-      last qstatLine if ( @tokens > 0 && ( $tokens[$jobID_Pos] eq $job_short ) );
-      }
+            # alebac: adapted the following lines to work at DCCN; stekai: works for DCC as well
+            # if not empty & job from list & still running
+            if ( @tokens > 0 && ( $tokens[$jobID_Pos] eq $job_short ) && ( $tokens[$statePos] ne "C" ) ) {
+
+                $jobsIncomplete = $jobsIncomplete + 1;
+
+                if ($verbose) {
+                    print "    Job $job is in state $tokens[$statePos]\n";
+                }
+            }
+            last qstatLine if ( @tokens > 0 && ( $tokens[$jobID_Pos] eq $job_short ) );
+        }
     }
 
+    if ($jobsIncomplete) {
+        if ($verbose) {
+            my $timestamp = `date`;
+            chomp $timestamp;
+            print "  ($timestamp) Still waiting for $jobsIncomplete jobs\n\n";
+        }
 
-  if( $jobsIncomplete )
-    {
-    if( $verbose )
-      {
-      my $timestamp = `date`;
-      chomp $timestamp;
-      print "  ($timestamp) Still waiting for $jobsIncomplete jobs\n\n";
-      }
-
-    # Use of backticks rather than system permits a ctrl+c to work
-    `sleep $delay`;
-    $qstatOutput = `qstat -u $user`;
-    @qstatLines = split("\n", $qstatOutput);
+        # Use of backticks rather than system permits a ctrl+c to work
+        `sleep $delay`;
+        $qstatOutput = `qstat -u $user`;
+        @qstatLines = split( "\n", $qstatOutput );
     }
-  }
+}
 
 if ($haveErrors) {
     print "  No more jobs to run - some jobs had errors\n\n";
@@ -141,9 +132,6 @@ else {
     print "  No more jobs in queue\n\n";
     exit 0;
 }
-
-
-
 
 sub trim {
 
